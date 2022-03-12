7a582c57d52499b3e065115cb80bd07ab07937f7
Reda BELHAJ <reda.belhaajj@gmail.com>
2022-03-12 12:28:34 +0100

    ➕ Add Functionnalities

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ def is_valid_file(parser, arg):
         return open(arg, 'r')
 
 def get_default_wordlist():
-    url = 'https://raw.githubusercontent.com/rbsec/dnscan/master/subdomains-1000.txt'
-    print(f'\u001b[33mUsing Default Wordlist you can found it here: {url}\u001b[0m \n')
+    url = 'https://raw.githubusercontent.com/Reda-BELHAJ/SuBrute/main/wordlist.txt'
+    print(f'[+] \u001b[33mUsing Default Wordlist you can found it here: {url}\u001b[0m \n')
 
     def_array   = []
     response    = urllib.request.urlopen(url)
@@ -27,21 +27,26 @@ def verbose_mode_check(args, sub, domain):
     if args.verbose:
         print(f'[-] {sub}.{domain} \u001b[31mInvalid\u001b[0m')
 
+def more_mode_check(args, ip_value):
+    if args.more:
+        print('\t\t |-', ip_value.rrset.to_text().replace('\n', '\n\t |- '))
+
 parser = argparse.ArgumentParser(description='\u001b[36mSuBrute is A Simple Subdomain Enumeration Tool \u001b[0m')
+
 parser.add_argument('Dom',
                        metavar='Domain',
                        type=str,
                        help='Domain Name of the Taget [ex : example.com]')
-parser.add_argument('-w', '--wordlist', 
+parser.add_argument('--wordlist', '-w',
                         nargs='?', 
-                        help='Wordlist', 
+                        help='Local wordlist path', 
                         metavar="FILE",
                         type=lambda x: is_valid_file(parser, x))
 parser.add_argument('--verbose', '-v',
                         help="Increase Output Verbosity",
                         action='store_true')
 parser.add_argument('--more', '-m',
-                        help="",
+                        help="Display more information about the Taget",
                         action='store_true')
 
 args            = parser.parse_args()
@@ -53,6 +58,8 @@ sub_array       = []
 
 wordlist_array = wordlist_file.read().splitlines() if wordlist_file else get_default_wordlist()
 
+print('[+] \u001b[36mEnumerating subdomains :\u001b[0m')
+
 for sub in wordlist_array:
     try:
         ip_value = dns.resolver.resolve(f'{sub}.{domain}', 'A')
@@ -60,10 +67,8 @@ for sub in wordlist_array:
         if ip_value:
             sub_array.append(f'{sub}.{domain}')
             if f"{sub}.{domain}" in sub_array:
-                print(f'[+] {sub}.{domain} \u001b[32mValid\u001b[0m')
-                if args.more:
-                    info = ip_value.rrset.to_text().replace('\n', '\n\t')
-                    print(f'\t{info}')
+                print(f'\t• {sub}.{domain} \u001b[32mValid\u001b[0m')
+                more_mode_check(args, ip_value)
         else:
             verbose_mode_check(args, sub, domain)
     except dns.resolver.NXDOMAIN:
@@ -72,3 +77,5 @@ for sub in wordlist_array:
         verbose_mode_check(args, sub, domain)
     except KeyboardInterrupt:
         quit()
+
+print(f'[+] \u001b[36mTotal Unique Subdomains Found: {len(sub_array)}\u001b[0m')
